8028c246986c462901392593ffd12b2f59ebe315
cholid_ridwan <cholid@id.edge-asia.com>
2016-06-21 14:47:24 +0700

    first commit

---
+++ app/src/main/java/com/example/cholid_ridwan/tescrashlytics/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.cholid_ridwan.tescrashlytics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+
+        // TODO: Move this to where you establish a user session
+        logUser();
+
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void forceCrash(View view) {
+        throw new RuntimeException("This is a crash");
+    }
+
+    private void logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        Crashlytics.setUserIdentifier("Alpha123456");
+        Crashlytics.setUserEmail("cholid@aleph-labs.com");
+        Crashlytics.setUserName("cholid");
+    }
+}
